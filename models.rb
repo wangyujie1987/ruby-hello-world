ac3c6833b043a1f2dc455ce33f8f77d41ee95a72
wangyujie1987 <41353213+wangyujie1987@users.noreply.github.com>
2019-03-02 10:11:09 +0800

    Update models.rb

--- models.rb
+++ models.rb
@@ -3,3 +3,6 @@ require 'active_record'
 class KeyPair < ActiveRecord::Base
     self.primary_key = 'key'
 end
+    <div class="page-header" align=center>
+      <h1> This is my custom demo! </h1>
+    </div>
